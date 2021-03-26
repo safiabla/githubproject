e2e93364f9771296cb16d28060ff299bdf34b7a8
smrtmrt601 <maratsafin601@gmail.com>
2021-03-26 08:49:04 -0400

    Add internal structure for backend

---
+++ code/backend/rider-svc/src/main/java/edu/githubproject/App.java
@@ -0,0 +1,13 @@
+package edu.githubproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
